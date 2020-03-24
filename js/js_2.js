ebad40f8da8622b8487cd898f3f84f87b30d1c12
Oleh Ilchyshyn <olehilchyshyn@MacBook-Air-Oleh.local>
2020-03-24 12:17:11 +0200

    test

--- js/js_2.js
+++ js/js_2.js
@@ -1,5 +1,14 @@
-let arr = ['My', 'age', 'is', 25];
-
-for(let i of arr) {
+function discountPrices (prices, discount) {
+    var discounted = [];
+    for (var i = 0; i < prices.length; i++) {
+        var discountedPrice = prices[i] * (1 - discount);
+        var finalPrice = Math.round(discountedPrice * 100) / 100;
+        discounted.push(finalPrice);
+    }
     console.log(i);
+    console.log(discountedPrice);
+    console.log(finalPrice);
+    return discounted
 }
+
+console.log(discountPrices([100, 200, 300], .033));
